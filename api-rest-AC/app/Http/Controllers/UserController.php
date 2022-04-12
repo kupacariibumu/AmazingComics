b23b861e3ff4c50417372351fb314cb649af1e74
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2022-04-12 15:29:35 -0500

    user manager works fine

--- api-rest-AC/app/Http/Controllers/UserController.php
+++ api-rest-AC/app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\UserManager;
 
 class UserController extends Controller
 {
@@ -18,59 +18,11 @@ class UserController extends Controller
         // $params = json_decode($json); // Se pueden tener los datos en forma de objeto
         $params_array = json_decode($json, true);
 
-        if(!empty($params_array)) {
-            // Limpiar los datos
-            $params_array = array_map('trim', $params_array);
+        // Instanciamos el gestor y delegamos la tarea de realizar el registro
+        $user_manager = new UserManager();
+        $data = $user_manager->register_user($params_array);
 
-            // Validar los datos
-            $validate = \Validator::make($params_array, [
-                'name'      => 'required|alpha',
-                'surname'   => 'required|alpha',
-                'email'     => 'required|email|unique:users',
-                'password'  => 'required'
-            ]);
-
-            if($validate->fails()) {
-                // ERROR: Los datos enviados no son correctos
-                $data = array(
-                    'status' => 'error',
-                    'code' => 400,
-                    'message' => 'Error con los datos',
-                    'errors' => $validate->errors()
-                );
-
-            } else {
-                // Cifrar la contraseña
-                $pass_hash = password_hash($params_array['password'], PASSWORD_BCRYPT, ['cost' => 4]);
-
-                // Crear el usuario
-                $user = new User();
-                $user->name = $params_array['name'];
-                $user->surname = $params_array['surname'];
-                $user->email = $params_array['email'];
-                $user->password = $pass_hash;
-                $user->role = 'ROLE_USER';
-
-                // Guardar el usuario
-                $user->save();
-
-                $data = array(
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'El usuario se ha creado correctamente',
-                    'user' => $user
-                );
-            }
-        } else {
-            // ERROR: No se enviaron datos
-            $data = array(
-                'status' => 'error',
-                'code' => 400,
-                'message' => 'No hay datos enviados',
-            );
-
-        }
-
+        // Devolvemos la respuesta del gestor
         return response()->json($data, $data['code']);
     }
 
---
+++ api-rest-AC/app/Models/UserManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class UserManager extends Model
+{
+    use HasFactory;
+
+    public function __construct() {}
+
+    public function register_user(Array $params_array) {
+        if(!empty($params_array)) {
+            // Limpiar los datos
+            $params_array = array_map('trim', $params_array);
+
+            // Validar los datos
+            $validate = \Validator::make($params_array, [
+                'name'      => 'required|alpha',
+                'surname'   => 'required|alpha',
+                'email'     => 'required|email|unique:users',
+                'password'  => 'required'
+            ]);
+
+            if($validate->fails()) {
+                // ERROR: Los datos enviados no son correctos
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'Error con los datos',
+                    'errors' => $validate->errors()
+                );
+
+            } else {
+                // Cifrar la contraseña
+                $pass_hash = password_hash($params_array['password'], PASSWORD_BCRYPT, ['cost' => 4]);
+
+                // Crear el usuario
+                $user = new User();
+                $user->name = $params_array['name'];
+                $user->surname = $params_array['surname'];
+                $user->email = $params_array['email'];
+                $user->password = $pass_hash;
+                $user->role = 'ROLE_USER';
+
+                // Guardar el usuario
+                $user->save();
+
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'El usuario se ha creado correctamente',
+                    'user' => $user
+                );
+            }
+        } else {
+            // ERROR: No se enviaron datos
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'No hay datos enviados',
+            );
+
+        }
+
+        return $data;
+    }
+
+}
