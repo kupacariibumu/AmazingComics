e5f0b5755573b05b50df2313291b417a13508510
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2022-11-22 21:44:48 -0600

    form register w/angular & some plus in web page

--- frontend-AC/src/app/components/register/register.component.ts
+++ frontend-AC/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user";
 
 @Component({
   selector: 'app-register',
@@ -8,14 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   public page_title: string;
+  public user: User;
 
   constructor() {
     this.page_title = "Registrate";
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
   }
 
   ngOnInit(): void {
   }
 
-
+  onSubmit(form: any) {
+    console.log(this.user);
+    form.reset();
+  }
 
 }
